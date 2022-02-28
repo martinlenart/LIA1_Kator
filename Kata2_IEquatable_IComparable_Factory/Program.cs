8af4c4726826ef56f8d32c3b2cfacb577108ee67
Martin Lenart <martin_lenart@icloud.com>
2022-02-28 08:26:50 +0100

    Add project files.

---
+++ Kata2_IEquatable_IComparable_Factory/Program.cs
@@ -0,0 +1,34 @@
+﻿//using Kata2_IEquatable_IComparable_Factory;
+
+#region Kata test of Member and MemberList
+//Write you code to test Member and MemberList here
+
+#endregion
+
+#region For Exercise: Refresh how to generate random initialization data
+Console.WriteLine(RandomDate());
+Console.WriteLine(RandomCity());
+
+static DateTime RandomDate()
+{
+    var rnd = new Random();
+    while (true)
+    {
+        try
+        {
+            int year = rnd.Next(1980, DateTime.Today.Year);
+            int month = rnd.Next(1, 13);
+            int day = rnd.Next(1, 32);
+
+            return new DateTime(year, month, day);
+        }
+        catch { }
+    }
+}
+static string RandomCity()
+{
+    var rnd = new Random();
+    string[] _cities = "Stockholm Göteborg Malmö".Split(' ');
+    return _cities[rnd.Next(0, _cities.Length)];
+}
+#endregion
